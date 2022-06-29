21ecfab5d2b380966b738fecae49849e3da8b3f5
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-06-29 19:01:03 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include<limits.h>
+#include<string.h>
+#include <math.h>
+ 
+#define loop(first,  size, decrement) for(int i=first; i < size; i+= decrement)
+#define fori(size) for(int i=0; i < size; i+= 1)
+#define forj(size) for(int j=0; j < size; j+= 1)
+ 
+#define int_i(x) scanf("%i", &x)
+#define int_o(x) printf("%i\n", x)
+ 
+#define long_i(x) scanf("%lld", &x)
+#define long_o(x) printf("%lld ", x)
+
+ 
+#define s_i(x) scanf("%s", x)
+#define s_o(x) printf("%s\n", x)
+ 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+
+
+
+void solve()
+{
+
+}
+ 
+ 
+
+ 
+ 
+int maxArry(int * arry, int size)
+{
+    int max = INT_MIN;
+    
+    fori(size)
+    {
+        if (arry[i] > max) max = arry[i];
+    }
+    return max;
+    
+}
+
+
+int minArry(int * arry, int size)
+{
+    int min = INT_MAX;
+    
+    fori(size)
+    {
+        if (arry[i] < min) min = arry[i];
+    }
+    return min;
+}
+
+ 
+ 
+int main()
+{
+    int N;
+    int_i(N);
+ 
+    
+    loop(0, N, 1) solve();
+    
+}
